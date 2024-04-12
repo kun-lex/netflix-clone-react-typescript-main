39724b4d65c3fa8b40c125d36777f6e872464025
Dev.David <akinola.funmi.2017@gmail.com>
2024-04-12 11:35:25 +0100

    update env file

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -28,9 +28,9 @@ export default function Footer() {
             target="_blank"
             underline="none"
             sx={{ color: "text.primary" }}
-            href="https://github.com/crazy-man22"
+            href="https://github.com/kun-lex"
           >
-            Crazy Man
+            Dev.david
           </Link>
         </Typography>
       </Divider>
